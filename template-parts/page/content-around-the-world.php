b03f67fd54c5da9457e54344b827e7c5c201bd2e
Eric Lobatos <eric@shopvida.com>
2018-04-27 10:41:10 -0700

    Finished picture grid

--- template-parts/page/content-around-the-world.php
+++ template-parts/page/content-around-the-world.php
@@ -44,55 +44,69 @@ global $twentyseventeencounter;
 	<?php endif; ?>
 </article><!-- #post-## -->
 
-<div class="container atw-info">
-	<h2>DREAMFLY PILLARS</h2>
-	<p>
-		Etiam eleifend egestas dui, iaculis euismod neque ultricies nec. Etiam ultrices porttitor purus. Duis posuere tempus leo, sed lobortis lacus. Ut finibus metus eros, in facilisis nibh ornare non.
-	</p>
-	<div class="row">
-		<div class="offset-2">
-			<h2>EDUCATION</h2>
-			<p>
-				To read, write, reason, and to question comfortable asumptions which inform beliefs.
-			</p>
-		</div>
-		<div class="offset-2">
-			<h2>EXPOSURE</h2>
-			<p>
-				To reach out to, to experience, and to understand the unknown enemy.
-			</p>
-		</div>
-		<div class="offset-2">
-			<h2>EMPOWERMENT</h2>
-			<p>
-				To imagine and realize a better reality for oneself and others.
-			</p>
-		</div>
-	</div>
-	<div id="img-container" class="row">
-		<div class="big-sqr-img imgs top-row-left">
-			<div class="country-title">
-				<h1>RWANDA</h1>
+<div class="atw-info">
+	<div class="container">
+		<h2>DREAMFLY PILLARS</h2>
+		<p>
+			Etiam eleifend egestas dui, iaculis euismod neque ultricies nec. Etiam ultrices porttitor purus. Duis posuere tempus leo, sed lobortis lacus. Ut finibus metus eros, in facilisis nibh ornare non.
+		</p>
+		<div class="row">
+			<div class="offset-2">
+				<h2>EDUCATION</h2>
+				<p>
+					To read, write, reason, and to question comfortable asumptions which inform beliefs.
+				</p>
+			</div>
+			<div class="offset-2">
+				<h2>EXPOSURE</h2>
+				<p>
+					To reach out to, to experience, and to understand the unknown enemy.
+				</p>
+			</div>
+			<div class="offset-2">
+				<h2>EMPOWERMENT</h2>
+				<p>
+					To imagine and realize a better reality for oneself and others.
+				</p>
 			</div>
 		</div>
-		<div class="imgs top-row-top-right d-none d-md-block"></div>
-		<div class="imgs top-row-bottom-right d-none d-md-block"></div>
-		<div class="long-big-img imgs bottom-row-top-left-pak">
-			<div class="country-title">
-				<h1>PAKISTAN</h1>
+		<div id="img-container" class="row">
+			<div class="big-sqr-img imgs top-row-left col-md-7">
+				<div class="country-title">
+					<h1>RWANDA</h1>
+				</div>
 			</div>
-		</div>
-		<div class="big-sqr-img imgs bottom-row-top-right-afghan t">
-			<div class="country-title">
-				<h1>AFGHANISTAN</h1>
+			<div class="sml-pair-imgs col-md-5 row">
+				<div class="imgs top-row-top-right d-none d-md-block col-md-12"></div>
+				<div class="imgs top-row-bottom-right d-none d-md-block col-md-12"></div>
 			</div>
-		</div>
-		<div class="imgs three-smiles d-none d-md-block"></div>
-		<div class="imgs bottom-row2-left d-none d-md-block"></div>
-		<div class="imgs bottom-row2-right d-none d-md-block"></div>
-		<div class="long-big-img imgs bottom-row-bottom-left-india">
-			<div class="country-title">
-				<h1>INDIA</h1>
+			<div class="big-sqr-img imgs bottom-row-top-left-pak col-md-5">
+				<div class="country-title">
+					<h1>PAKISTAN</h1>
+				</div>
+			</div>
+			
+			<div class="big-pair-imgs col-md-7 row">
+				<div class="imgs bottom-row-top-right-afghan t d-none d-md-block col-md-12">
+					<div style="position: relative;"class="country-title">
+						<h1>AFGHANISTAN</h1>
+					</div>
+				</div>
+				<div class="imgs three-smiles d-none d-md-block col-md-12"></div>
+			</div>
+			<div class="big-sqr-img imgs bottom-row-top-right-afghan t d-block d-sm-none">
+				<div class="country-title">
+					<h1>AFGHANISTAN</h1>
+				</div>
+			</div>
+			<div class="big-pair-imgs col-md-7 row">
+				<div class="imgs bottom-row2-left d-none d-md-block col-md-12"></div>
+				<div class="imgs bottom-row2-right d-none d-md-block col-md-12"></div>
+			</div>
+			<div class="big-sqr-img imgs bottom-row-bottom-left-india col-md-5">
+				<div class="country-title">
+					<h1>INDIA</h1>
+				</div>
 			</div>
 		</div>
 	</div>
